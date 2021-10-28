07b4f95e4cd6eb2bc8ceef0b2c24a44f0262c83f
Mostafa-hemma <76970262+Mostafa-hemma@users.noreply.github.com>
2021-10-28 19:12:03 +0200

    ex.3 solved

---
+++ C_Programming/HW1/EX3/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: mosta
+ */
+
+#include "stdio.h"
+
+void main(){
+
+	int x=0;
+	int y=0;
+	int sum=0;
+
+	printf("Enter two integers: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&x);
+	scanf("%d",&y);
+	printf("sum is: %d",x+y);
+
+
+}
