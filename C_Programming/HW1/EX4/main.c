bc028914d02e225453734aeb363b2ebf7770c008
Mostafa-hemma <76970262+Mostafa-hemma@users.noreply.github.com>
2021-10-28 19:20:13 +0200

    ex4 solved

---
+++ C_Programming/HW1/EX4/main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: mosta
+ */
+
+
+#include "stdio.h"
+
+void main(){
+
+	float x=0;
+	float y=0;
+	float sum=0;
+
+	printf("Enter two numbers: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%f",&x);
+	scanf("%f",&y);
+	printf("sum is: %f",x*y);
+
+
+}
