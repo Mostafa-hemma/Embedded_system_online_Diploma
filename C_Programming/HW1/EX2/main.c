b6b41e53d41308f82bb779055b92b083ed6cc571
Mostafa-hemma <76970262+Mostafa-hemma@users.noreply.github.com>
2021-10-28 19:06:30 +0200

    ssolving the second exercise

---
+++ C_Programming/HW1/EX2/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: mosta
+ */
+
+#include "stdio.h"
+
+void main(){
+	int x=0;
+	printf("Enter a integer: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&x);
+	printf("\nYou entered: %d",x);
+}
