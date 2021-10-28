fd6800095c8e054f9ad368b19fcaf49482950c2f
Mostafa-hemma <76970262+Mostafa-hemma@users.noreply.github.com>
2021-10-28 19:23:48 +0200

    ex5 solved

---
+++ C_Programming/HW1/EX5/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: mosta
+ */
+
+
+#include "stdio.h"
+
+void main(){
+
+	char x;
+
+	printf("Enter a Character: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%c",&x);
+
+	printf("sum is: %d",x);
+
+
+}
