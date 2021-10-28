e180e460a8a5fbb2f2a4434e30679130318c0c5f
Mostafa-hemma <76970262+Mostafa-hemma@users.noreply.github.com>
2021-10-28 20:02:13 +0200

    adding solution of ex 6 and 7

---
+++ C_Programming/HW1/EX6/main.c
@@ -0,0 +1,34 @@
+/*
+ * main.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: mosta
+ */
+
+
+#include "stdio.h"
+
+void main(){
+
+	float a=0;
+	float b=0;
+	printf("Enter value of a: ");
+	fflush(stdin);fflush(stdout);
+
+	scanf("%f",&a);
+	fflush(stdin);fflush(stdout);
+
+	printf("Enter value of b: ");
+	fflush(stdin);fflush(stdout);
+
+	scanf("%f",&b);
+	//a=20
+	//b=60
+	//a=60  b=20
+	a=a*b; //a=1200
+	b=a/b;  // 1200/60=20
+	a=a/b;
+	printf("After swapping, value of a =  %f",a,"\n");
+	printf("\nAfter swapping, value of a =  %f",b);
+
+}
